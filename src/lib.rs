b5a9ee0eade3893ca93aea36832590d17789b218
Thomas Nicollet <thomas.nicollet@epitech.eu>
2020-11-12 14:32:58 +0900

    :sparkles: Added old spatial-sdk-rs version

---
+++ src/lib.rs
@@ -0,0 +1,169 @@
+use spatialos_sys::schema::ComponentData;
+use spatialos_sys::schema::ComponentUpdate;
+use spatialos_sys::worker::component_vtable::ComponentVtable;
+use spatialos_sys::worker::ComponentDataHandle;
+use spatialos_sys::worker::ComponentId;
+use spatialos_sys::worker::ComponentUpdateHandle;
+use spatialos_sys::Schema_ComponentData;
+use spatialos_sys::Schema_ComponentUpdate;
+use std::os::raw::c_void;
+
+pub trait Component: Clone {
+    type Update: Clone;
+
+    const ID: ComponentId;
+
+    fn component_data_deserialize(
+        component_id: ComponentId,
+        user_data: *mut c_void,
+        source: ComponentData,
+    ) -> Self;
+
+    fn component_data_serialize(
+        component_id: ComponentId,
+        user_data: *mut c_void,
+        handle: &mut Self,
+    ) -> ComponentData;
+
+    fn component_update_deserialize(
+        component_id: ComponentId,
+        user_data: *mut c_void,
+        source: ComponentUpdate,
+    ) -> Self::Update;
+
+    fn component_update_serialize(
+        component_id: ComponentId,
+        user_data: *mut c_void,
+        handle: &mut Self::Update,
+    ) -> ComponentUpdate;
+
+    fn component_update_free(
+        component_id: ComponentId,
+        user_data: *mut c_void,
+        handle: Self::Update,
+    );
+
+    fn component_update_copy(
+        component_id: ComponentId,
+        user_data: *mut c_void,
+        handle: &Self::Update,
+    ) -> Self::Update;
+
+    fn get_vtable() -> ComponentVtable {
+        ComponentVtable {
+            component_id: Self::ID,
+            user_data: std::ptr::null_mut(),
+            command_request_free: None,
+            command_request_copy: None,
+            command_request_deserialize: None,
+            command_request_serialize: None,
+            command_response_free: None,
+            command_response_copy: None,
+            command_response_deserialize: None,
+            command_response_serialize: None,
+            component_data_free: Some(component_data_free::<Self>),
+            component_data_copy: Some(component_data_copy::<Self>),
+            component_data_deserialize: Some(component_data_deserialize::<Self>),
+            component_data_serialize: Some(component_data_serialize::<Self>),
+            component_update_free: Some(component_update_free::<Self>),
+            component_update_copy: Some(component_update_copy::<Self>),
+            component_update_deserialize: Some(component_update_deserialize::<Self>),
+            component_update_serialize: Some(component_update_serialize::<Self>),
+        }
+    }
+}
+
+
+pub unsafe extern "C" fn component_data_deserialize<T: Component>(
+    component_id: ComponentId,
+    user_data: *mut c_void,
+    source: *mut Schema_ComponentData,
+    handle_out: *mut *mut ComponentDataHandle,
+) -> u8 {
+    let source = source.into();
+    let new_data = Box::new(T::component_data_deserialize(
+        component_id,
+        user_data,
+        source,
+    ));
+    *(handle_out as *mut *mut T) = Box::into_raw(new_data);
+    1
+}
+
+pub unsafe extern "C" fn component_data_serialize<T: Component>(
+    component_id: ComponentId,
+    user_data: *mut c_void,
+    handle: *mut ComponentDataHandle,
+    target_out: *mut *mut Schema_ComponentData,
+) {
+    let handle = handle as *mut T;
+    let mut data = Box::from_raw(handle);
+    let component_data = T::component_data_serialize(component_id, user_data, &mut *data).into();
+    *target_out = component_data;
+    Box::into_raw(data);
+}
+
+pub unsafe extern "C" fn component_update_deserialize<T: Component>(
+    _: ComponentId,
+    _: *mut c_void,
+    _source: *mut Schema_ComponentUpdate,
+    _handle_out: *mut *mut ComponentUpdateHandle,
+) -> u8 {
+    unimplemented!()
+}
+
+pub unsafe extern "C" fn component_update_serialize<T: Component>(
+    _: ComponentId,
+    _: *mut c_void,
+    _handle: *mut ComponentUpdateHandle,
+    _target_out: *mut *mut Schema_ComponentUpdate,
+) {
+    unimplemented!()
+}
+
+pub unsafe extern "C" fn component_data_copy<T: Component>(
+    _: ComponentId,
+    _: *mut c_void,
+    handle: *mut ComponentDataHandle,
+) -> *mut ComponentDataHandle {
+    let handle = handle as *mut T;
+    let ptr = Box::from_raw(handle);
+    let new_data = ptr.clone();
+    Box::into_raw(ptr);
+    Box::into_raw(new_data) as *mut ComponentDataHandle
+}
+
+pub unsafe extern "C" fn component_data_free<T>(
+    _: ComponentId,
+    _: *mut c_void,
+    handle: *mut ComponentDataHandle,
+) {
+    Box::from_raw(handle);
+}
+
+pub unsafe extern "C" fn component_update_free<T: Component>(
+    component_id: ComponentId,
+    user_data: *mut c_void,
+    handle: *mut ComponentUpdateHandle,
+) {
+    let handle = handle as *mut T::Update;
+    let handle = Box::from_raw(handle);
+    T::component_update_free(component_id, user_data, *handle)
+}
+
+pub unsafe extern "C" fn component_update_copy<T: Component>(
+    component_id: ComponentId,
+    user_data: *mut c_void,
+    handle: *mut ComponentUpdateHandle,
+) -> *mut ComponentUpdateHandle {
+    let handle = handle as *mut T::Update;
+    let ptr = Box::from_raw(handle);
+    let new_data = Box::new(T::component_update_copy(component_id, user_data, &*ptr));
+
+    Box::into_raw(ptr);
+    Box::into_raw(new_data) as *mut ComponentDataHandle
+}
+
+pub mod sys_exports {
+    pub use spatialos_sys::*;
+}
